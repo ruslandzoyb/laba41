ebe93a425e4ef9012a96ef73989da0f3c5eeaf1a
rusya <dnepr.ukrainec@gmail.com>
2019-10-20 15:55:37 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Class1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class ThreadArray
+    {
+
+        private Thread[] Threads;
+        private int[] Array;
+        private readonly int arrSize;
+        private readonly int threadCount;
+        private readonly int proccesor;
+        private int valForThread;
+        private static Random r = new Random();
+        private int start = 0;
+        private int end = 0;
+        public int begin=0;
+        public DateTime nev;
+        public DateTime off;
+        static object locker = new object();
+
+        public ThreadArray(int arrSize, int threadCount, int proccesor)
+        {
+            if (arrSize <= 0 || threadCount <= 0 || proccesor <= 0)
+            {
+                throw new Exception("Менее ,или == 0");
+            }
+            this.arrSize = arrSize;
+            this.threadCount = threadCount;
+            this.proccesor = proccesor;
+
+            if (arrSize % threadCount == 0)
+            {
+                valForThread = arrSize / threadCount;
+            }
+            else
+            {
+                throw new Exception("");
+            }
+            Threads = new Thread[threadCount];
+            Array = new int[arrSize];
+
+            for (int i = 0; i < Threads.Length; i++)
+            {
+                Threads[i] = new Thread(Initiliaz);
+                Threads[i].Name = "Thread " + i.ToString();
+                //
+
+
+            }
+        }
+        public void Run()
+            
+
+
+        {
+           
+           
+            nev = DateTime.Now;
+            Parallel.For(0, Threads.Length, new ParallelOptions() { MaxDegreeOfParallelism = proccesor }, i =>
+            {
+
+                Threads[i].Start();
+               
+            }
+
+
+            );
+
+
+
+            off = DateTime.Now;
+            Time();
+        }
+        public void Time()
+        {
+            Console.WriteLine(off.Millisecond-nev.Millisecond);
+           
+        }
+
+        private void Initiliaz()
+        {
+
+
+
+            lock (locker)
+            {
+                end += valForThread;
+                for (int j = start; j < end; j++)
+                {
+                    Array[j] = r.Next(1, 10);
+
+                    Console.WriteLine($"{j} {Array[j]} {Thread.CurrentThread.Name}");
+
+                }
+                start = end;
+            }
+
+            
+
+
+        }
+
+        public int this [int index]
+        {
+            get
+            {
+                return Array[index];
+            }
+        }
+
+
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // Thread.CurrentThread.Name = "Main";
+            ThreadArray thread = new ThreadArray(100, 5,4);
+            thread.Run();
+            
+            Thread.Sleep(200);
+            //thread.Time();
+          //  Console.WriteLine( $"Time {thread.nev.ToLongTimeString()} + {thread.off.ToLongTimeString()} ");
+
+
+           /* for (int i = 0; i < 150; i++)
+            {
+                Console.WriteLine($"{thread[i]} {i}");
+            }*/
+
+            Console.ReadKey();
+        }
+    }
+}
